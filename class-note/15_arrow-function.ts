de4f9d49a6138678b96247d16e6fa7694fa10748
LeeHwanWoong <lhw3110@gmail.com>
2021-01-07 23:38:52 +0900

    add arrow-fuction

---
+++ class-note/15_arrow-function.ts
@@ -0,0 +1,17 @@
+funciton sum(a, b){
+    return a + b;
+}
+
+var sum = function(a,b){
+    return a+b;
+}
+
+var summ = (a,b) => {
+    return a+b;
+}
+
+var sum = (a,b) => a+b;
+
+var summ = (a: number,b: number): number => {
+    return a+b;
+}
